6fc9073a87ba8dea6ff0822a66c287359b6a96c8
The-Jackrabbit <lsm5fm@virginia.edu>
2018-07-21 10:58:56 -0700

    Added linting, routing, and state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+// Pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import StaffLogin from './pages/StaffLogin';
+import PageNotFound from './pages/PageNotFound';
+
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/contact/" component={Contact} />
+        <Route exact path="/login/" component={StaffLogin} />
+        <Route path="/" component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import createHistory from 'history/createBrowserHistory';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { Route } from 'react-router';
+import store from './redux/store';
 import App from './App';
 
+const history = createHistory();
+
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
 );
