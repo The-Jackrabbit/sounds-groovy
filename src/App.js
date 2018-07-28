446b0550d36b037b7645844cb539ff303178d2a1
Michele Masters <michelemasters@Micheles-MacBook-Pro.local>
2018-07-27 20:17:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import AboutUs from './pages/AboutUs/AboutUs';
-import PageNotFound from './pages/PageNotFound';
+import Header from './pages/Header/Header';
 
 const App = () => {
   return (
     <div>
+        <Header/>
+        <AboutUs/>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/contact/" component={Contact} />
         <Route exact path="/aboutus/" component={AboutUs} />
-        <Route path="/" component={PageNotFound} />
       </Switch>
     </div>
   );
