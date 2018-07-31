4c4839460ca973c68a4bb597e5c8c6e931c3cb17
The-Jackrabbit <lsm5fm@virginia.edu>
2018-07-30 21:53:31 -0700

    Initial commit - moved css to colors to var

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,26 +6,24 @@ import logo from '../../resources/logo.svg';
 
 const Header = () => {
   return (
-    <div>
-      <div className="header-container">
-        <Link to='/' style={{verticalAlign: 'initial'}}>
-          <img src={logo} alt="Sounds Groovy OBX" height="60" width="40" align='middle' />
-        </Link>
-        <Link to='/aboutus'>About Us</Link>
-        <Link to='/location'>Location</Link>
-        <Link to='/gallery'>Gallery</Link>
-        <Link to='/amenities'>Amenities</Link>
-        <Link to='/rules'>Rules, Groovy!</Link>
-        <Link to='/guide'>OBX Guide</Link>
-        <Link to='/guests'>Groovy Guests!</Link>
-        <Link to='/faq'>FAQ</Link>
-        <Link to='/blog'>BLOG</Link>
-        <Link to='/contactus'>Contact Us</Link>
-        {/* 
-        <Link to='/login'>Staff Login</Link>
-        <Link to='/aadsfasdfsd'>404 Example</Link>
-        */}
-      </div>
+    <div className="header-container">
+      <Link to='/' style={{verticalAlign: 'initial'}}>
+        <img src={logo} alt="Sounds Groovy OBX" height="60" width="40" align='middle' />
+      </Link>
+      <Link to='/aboutus'>About Us</Link>
+      <Link to='/location'>Location</Link>
+      <Link to='/gallery'>Gallery</Link>
+      <Link to='/amenities'>Amenities</Link>
+      <Link to='/rules'>Rules, Groovy!</Link>
+      <Link to='/guide'>OBX Guide</Link>
+      <Link to='/guests'>Groovy Guests!</Link>
+      <Link to='/faq'>FAQ</Link>
+      <Link to='/blog'>BLOG</Link>
+      <Link to='/contactus'>Contact Us</Link>
+      {/* 
+      <Link to='/login'>Staff Login</Link>
+      <Link to='/aadsfasdfsd'>404 Example</Link>
+      */}
     </div>
   );
 };
