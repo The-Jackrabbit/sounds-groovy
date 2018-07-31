b54e86d3faab601b42938f45d7b460c4da3fe7df
Michele <MasterWorksHappy@gmail.com>
2018-07-31 18:16:22 -0400

    Started to setup Guide but need advice on approach

--- src/pages/guide/Guide.js
+++ src/pages/guide/Guide.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import deck_life from '../../resources/images/0622181427-EFFECTS.png';
+import './Guide.css';
+import sunset from '../../resources/images/0628182027b.png';
+
 
 const Guide = () => {
+  // Luke for this one I want something like this ...
+  // https://v2.hostfully.com/gsntfbq/recommendations/food
+  // essentially a photo and a link and a recco blurb
   return (
-    <div>
-      <h1>ABOUT US</h1>
-      <img src={deck_life} alt="deck-life"/>
-      <p>
-        We are the Masters Family!
-      </p>
-      <p>
-        Why Sounds Groovy?  For over 30 years this was the home to the coolest, most loving grandparents ever!  They were wonderful free spirits!  Sounds Groovy is an acknowledgement of all the beauty and kindness they shared while they were with us.  They enjoyed fishing, beaching, and love and laughter.  Hopefully you will experience that and much more when you visit Sounds Groovy!
-      </p>
-      <p>
-        Sounds Groovy will be undergoing renovations starting October 2018.  So, during your visit you may notice nail holes, scuffed walls, dated carpet, bare walls and paint samples on walls (see Office bedroom photos).  But, the beds are comfortable and the house offers many amenities to make your visit enjoyable.  The renovation will include a new kitchen, flooring throughout, enhanced outdoor living spaces, new outdoor stairs and deck railing.  Do you have suggestions for improvements we might make during the renovation?  Let us know!
-      </p>
+    <div className="Guide">
+      <img className="AboutUs-img" alt="AboutUs-img" src={sunset} />
+      <h2 align="center">
+        Our Favorite Places in the Outer Banks!
+      </h2>
+
     </div>
   );
 };
