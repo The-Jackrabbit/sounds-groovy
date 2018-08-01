a15106798795d282305742ec3bdec5f0e7b3c273
The-Jackrabbit <lsm5fm@virginia.edu>
2018-07-31 21:25:57 -0700

    Changed the color of the link for the current page to provide context

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,31 +1,37 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import logo from '../../resources/logo.svg';
+import CustomLink from './Link';
 import './Header.css';
 
-import { Link } from 'react-router-dom';
-import logo from '../../resources/logo.svg';
+const propTypes = {
+  location: PropTypes.string,
+};
 
-const Header = () => {
+const Header = ({ location }) => {
+  const current = location.pathname;
+
   return (
     <div className="header-container">
       <Link to='/' style={{verticalAlign: 'initial'}}>
         <img src={logo} alt="Sounds Groovy OBX" height="60" width="40" align='middle' />
       </Link>
-      <Link to='/aboutus'>About Us</Link>
-      <Link to='/location'>Location</Link>
-      <Link to='/gallery'>Gallery</Link>
-      <Link to='/amenities'>Amenities</Link>
-      <Link to='/rules'>Rules, Groovy!</Link>
-      <Link to='/guide'>OBX Guide</Link>
-      <Link to='/guests'>Groovy Guests!</Link>
-      <Link to='/faq'>FAQ</Link>
-      <Link to='/blog'>BLOG</Link>
-      <Link to='/contactus'>Contact Us</Link>
-      {/* 
-      <Link to='/login'>Staff Login</Link>
-      <Link to='/aadsfasdfsd'>404 Example</Link>
-      */}
+      <CustomLink to='/aboutus' current={current}>About Us</CustomLink>
+      <CustomLink to='/location' current={current}>Location</CustomLink>
+      <CustomLink to='/gallery' current={current}>Gallery</CustomLink>
+      <CustomLink to='/amenities' current={current}>Amenities</CustomLink>
+      <CustomLink to='/rules' current={current}>Rules, Groovy!</CustomLink>
+      <CustomLink to='/guide' current={current}>OBX Guide</CustomLink>
+      <CustomLink to='/guests' current={current}>Groovy Guests!</CustomLink>
+      <CustomLink to='/faq' current={current}>FAQ</CustomLink>
+      <CustomLink to='/blog' current={current}>BLOG</CustomLink>
+      <CustomLink to='/contactus' current={current}>Contact Us</CustomLink>
     </div>
   );
 };
 
-export default Header;
+Header.propTypes = propTypes;
+
+export default withRouter((props) => <Header {...props} />);
